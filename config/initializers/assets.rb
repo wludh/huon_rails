c2ff95a162ed10a1c73b61548b2812cb814a4031
Brandon Walsh <walshb@wlu.edu>
2016-03-29 10:34:30 -0400

    continues to clean up repo

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,5 +15,4 @@ Rails.application.config.assets.precompile += %w( base_styles.css )
 Rails.application.config.assets.precompile += %w( app.css )
 Rails.application.config.assets.precompile += %w( tei_styles.css )
 
-# Rails.application.config.assets.precompile += %w( vmachine.js )
-# Rails.application.config.assets.precompile += %w( vmachine_scripts.js )
+Rails.application.config.assets.precompile += %w( vmachine.js )
