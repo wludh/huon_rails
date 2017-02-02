75789dc0a5a5f14a31469f7d7e4d303aa4e94a97
Brandon Walsh <bmw9t@virginia.edu>
2017-02-02 09:21:20 -0500

    refactoring

--- app/assets/javascripts/sitewide/browse.js
+++ app/assets/javascripts/sitewide/browse.js
@@ -7,13 +7,6 @@ var ready;
         }
     }
 
-    // function hide_footer(){
-    //     // hides the footer
-    //     if(document.getElementById('TEI_reader') !== null){
-    //         $(".footer").css('display','none');
-    //     }
-    // }
-
     function assign_note_numbers(){
         // gets the xml id of the nodes and assigns them once the page has loaded.
             $.each($('sup'), function(item){
@@ -38,20 +31,6 @@ var ready;
         });
     });
 
-    // $(function annotation_reveal(){
-    //     // reveal an annotation when clicked
-    //     $('sup').click(function(){
-    //         hide_non_active_panels();
-    //         $('.note-contents note').addClass('hidden');
-    //         var annotation_number = this.parentNode.getAttribute('rightnum');
-    //         $('#note-header').addClass('visible');
-    //         $("note[xml=" + String(annotation_number) + "]").addClass("visible");
-    //         $('.note-contents.hidden').removeClass('hidden');
-    //         $('#side-panel').css('display', 'block');
-    //         open_sidepanel();
-    //     });
-    // });
-
     function open_sidepanel(){
         $('#TEI_reader').unwrap();
         $('#TEI_reader').css('margin-left', '0%');
@@ -65,22 +44,11 @@ var ready;
         $('#ms-image-container').hide();
     }
 
-  // function hide_image(){
-  //       $('#image-header').removeClass('visible');
-  //       $('#ms-image').removeClass('visible');
-  //       $('#ms-image-container').removeClass('visible');
-  //   }
-
     function hide_notes(){
         $('note.active-element').hide();
         $('note.active-element').removeClass('active-element');
         $('#note-header').hide();
     }
-
-    // function hide_notes(){
-    //     $('note.visible').removeClass('visible');
-    //     $('#note-header').removeClass('visible');
-    // }
 
     function hide_tei(){
         $('#tei-embed-container').hide();
@@ -88,13 +56,6 @@ var ready;
         $('#tei-embed-header').removeClass('active-element');
         $('#tei-embed').hide();
     }
-
-    // function hide_tei(){
-    //     $('#tei-embed-container').removeClass('visible');
-    //     $('#tei-embed-header').removeClass('visible');
-    //     $('#tei-embed').removeClass('visible');
-    // }
-
 
     $(function close_sidepanel(){
         // close the notes panel when the x is clicked
@@ -132,23 +93,10 @@ var ready;
         $('sic').show();
     }
 
-    // function toggle_sic_on(){
-    //     $("corr").removeClass('visible');
-    //     $('sic').addClass('visible');
-    // }
-
-
     function toggle_sic_off(){
         $('sic').hide();
         $("corr").show();
     }
-
-
-    // function toggle_sic_off(){
-    //     $('sic').removeClass('visible');
-    //     $("corr").addClass('visible');
-    // }
-
 
     function page_prep(){
         // prepare the page on load
@@ -195,23 +143,6 @@ var ready;
         });
     });
 
-    // $(function image_reveal(){
-    //     $('#image-reveal').click(function(){
-    //         hide_non_active_panels();
-    //         $('#image-header').addClass('visible');
-    //         $('#ms-image-container').addClass('visible');
-    //         $('#ms-image').addClass('visible');
-    //         $('#side-panel').css('display', 'block');
-    //         $('#ms-image').css('display', 'block !important');
-    //         open_sidepanel();
-    //     });
-    // });
-
-    // $('#tei-reveal').click(function(){
-    //         $('#loading-gif').css('display', 'inline');
-    //         $('#loading-gif').addClass('visible');
-    // });
-
     $(function tei_reveal(){
         $('#tei-reveal').click(function(){
             // $('#loading-gif').css('display', 'inline');
@@ -241,35 +172,6 @@ var ready;
             });
         });
     });
-
-    // $(function tei_reveal(){
-    //     $('#tei-reveal').click(function(){
-    //         // $('#loading-gif').css('display', 'inline');
-    //         // $('#loading-gif').addClass('visible');
-    //         // $('#loading-gif').html('<img src="https://raw.githubusercontent.com/wludh/huon_rails/master/app/assets/images/ajax-loader.gif" />');
-    //         $('#tei-embed-header').addClass('visible');
-    //         $('#side-panel').css('display', 'block');
-    //         $('#tei-embed-container').addClass('visible');
-    //         $('#tei-embed-header').text('Loading TEI');
-    //         $('#tei-embed-container').css('display', 'inline');
-    //         hide_non_active_panels();
-    //         open_sidepanel();
-
-    //         $.ajax({
-    //             success: function(){
-    //                 $('#tei-embed-header').text('TEI');
-    //                 $('#tei-embed').addClass('visible');
-    //                 $('#tei-embed').css('display', 'block');
-    //             }
-    //         });
-    //         $.ajax({
-    //             success:function(){
-    //                 $('#loading-gif').removeClass('visible');
-    //                 $('#loading-gif').css('display', 'none');
-    //             }
-    //         });
-    //     });
-    // });
     page_prep();
 };
 
